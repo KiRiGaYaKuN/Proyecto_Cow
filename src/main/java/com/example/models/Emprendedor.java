08246d69ed7ebbeda2ed4406f611aa125ef48004
KiRiGaYaKuN <cristianpatibasti@gmail.com>
2022-05-09 17:10:10 -0500

    V1.2

--- src/main/java/com/example/models/Emprendedor.java
+++ src/main/java/com/example/models/Emprendedor.java
@@ -22,7 +22,14 @@ import javax.persistence.TemporalType;
 
 
 @Entity
-public class Emprendedor {
+public class Emprendedor implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
     
     
 }
--- src/main/java/com/example/models/Proyecto.java
+++ src/main/java/com/example/models/Proyecto.java
@@ -4,10 +4,93 @@
  */
 package com.example.models;
 
-/**
- *
- * @author cristian-patino
- */
-public class Proyecto {
+import com.sun.istack.NotNull;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Set;
+import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+
+@Entity
+public class Proyecto implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotNull
+    @Column(name = "iniciodate", updatable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar iniciodate;
+       
+    
+    private Calendar finaldate;
+    
+    private String nombre;    
+    
+    private String descripcion;
+    
+    private String responsable;
+
+    public Proyecto() {
+    }
+
+    @PrePersist
+    private void creationTimestamp() {
+    this.iniciodate = Calendar.getInstance();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Calendar getFinaldate() {
+        return finaldate;
+    }
+
+    public void setFinaldate(Calendar finaldate) {
+        this.finaldate = finaldate;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getResponsable() {
+        return responsable;
+    }
+
+    public void setResponsable(String responsable) {
+        this.responsable = responsable;
+    }
+    
     
 }
