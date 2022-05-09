ef2b8d7928533acfb8bd449c04f15f4ea0700bd6
KiRiGaYaKuN <cristianpatibasti@gmail.com>
2022-05-09 17:29:44 -0500

    V1.3

--- src/main/java/com/example/models/Donante.java
+++ src/main/java/com/example/models/Donante.java
@@ -10,4 +10,5 @@ package com.example.models;
  */
 public class Donante {
     
+    
 }
--- src/main/java/com/example/models/ProyectoAsignado.java
+++ src/main/java/com/example/models/ProyectoAsignado.java
@@ -4,10 +4,83 @@
  */
 package com.example.models;
 
-/**
- *
- * @author cristian-patino
- */
-public class ProyectoAsignado {
+import com.sun.istack.NotNull;
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.Set;
+import static javax.persistence.CascadeType.ALL;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class ProyectoAsignado implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotNull
+    @Column(name = "fecha", updatable = false)
+    @Temporal(TemporalType.DATE)
+    private Calendar fecha;
+    
+    private Long idpostulante;
+    private Long idproyecto;    
+    private String estado;
+    private int valor;
+
+    public ProyectoAsignado() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdpostulante() {
+        return idpostulante;
+    }
+
+    public void setIdpostulante(Long idpostulante) {
+        this.idpostulante = idpostulante;
+    }
+
+    public Long getIdproyecto() {
+        return idproyecto;
+    }
+
+    public void setIdproyecto(Long idproyecto) {
+        this.idproyecto = idproyecto;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+    
     
 }
